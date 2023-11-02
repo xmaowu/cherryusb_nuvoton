b10bd59316dc64242310be8bcce1e0da441cfa61
Yang Xijing <814370731@qq.com>
2023-11-02 11:30:32 +0800

    M031 Series

---
+++ M031_Series_BSP_CMSIS_V3.05.000/SampleCode/cdc_acm_template/board.c
@@ -0,0 +1,113 @@
+#include <rthw.h>
+#include <rtthread.h>
+#include <stdint.h>
+#include "NuMicro.h"
+
+#define CRYSTAL_LESS 0
+
+void SYS_Init(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Enable HIRC clock */
+    CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk);
+
+    /* Waiting for HIRC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+
+    /* Switch HCLK clock source to HIRC and HCLK source divide 1 */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC, CLK_CLKDIV0_HCLK(1));
+
+    /* Switch UART0 clock source to HIRC */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HIRC, CLK_CLKDIV0_UART0(1));
+
+    /* Enable UART0 clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+
+    /* Switch USB clock source to HIRC & USB Clock = HIRC / 1 */
+    CLK_SetModuleClock(USBD_MODULE, CLK_CLKSEL0_USBDSEL_HIRC, CLK_CLKDIV0_USB(1));
+
+    /* Enable USB clock */
+    CLK_EnableModuleClock(USBD_MODULE);
+
+    /* Update System Core Clock */
+    SystemCoreClockUpdate();
+
+    /* Set PB multi-function pins for UART0 RXD=PB.12 and TXD=PB.13 */
+    SYS->GPB_MFPH = (SYS->GPB_MFPH & ~(SYS_GPB_MFPH_PB12MFP_Msk | SYS_GPB_MFPH_PB13MFP_Msk))
+                    |(SYS_GPB_MFPH_PB12MFP_UART0_RXD | SYS_GPB_MFPH_PB13MFP_UART0_TXD);
+
+    /* Lock protected registers */
+    SYS_LockReg();
+}
+
+#if defined(RT_USING_USER_MAIN) && defined(RT_USING_HEAP)
+/*
+ * Please modify RT_HEAP_SIZE if you enable RT_USING_HEAP
+ * the RT_HEAP_SIZE max value = (sram size - ZI size), 1024 means 1024 bytes
+ */
+#define RT_HEAP_SIZE (15 * 1024)
+static rt_uint8_t rt_heap[RT_HEAP_SIZE];
+
+RT_WEAK void *rt_heap_begin_get(void)
+{
+    return rt_heap;
+}
+
+RT_WEAK void *rt_heap_end_get(void)
+{
+    return rt_heap + RT_HEAP_SIZE;
+}
+#endif
+
+void SysTick_Handler(void)
+{
+    rt_interrupt_enter();
+
+    rt_tick_increase();
+
+    rt_interrupt_leave();
+}
+
+/**
+ * This function will initial your board.
+ */
+void rt_hw_board_init(void)
+{
+    SYS_Init();
+
+    SysTick_Config(SystemCoreClock / RT_TICK_PER_SECOND);
+
+    /* Call components board initial (use INIT_BOARD_EXPORT()) */
+#ifdef RT_USING_COMPONENTS_INIT
+    rt_components_board_init();
+#endif
+
+#if defined(RT_USING_USER_MAIN) && defined(RT_USING_HEAP)
+    rt_system_heap_init(rt_heap_begin_get(), rt_heap_end_get());
+#endif
+}
+
+#ifdef RT_USING_CONSOLE
+
+static int rt_hw_console_init(void)
+{
+    UART_Open(UART0, 2000000);
+    return 0;
+}
+INIT_BOARD_EXPORT(rt_hw_console_init);
+
+void rt_hw_console_output(const char *str)
+{
+    UART_Write(UART0, (uint8_t *)str, rt_strlen(str));
+}
+
+static int system_core_clock_log(void)
+{
+    rt_kprintf("SystemCoreClock: %dHz\n", SystemCoreClock);
+    return 0;
+}
+INIT_PREV_EXPORT(system_core_clock_log);
+
+#endif
